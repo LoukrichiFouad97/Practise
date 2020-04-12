c612fa6d19b3964a8e06e02e04fd0ab4e4d7cc54
fouad97 <loukrichi21@gmail.com>
2020-04-12 22:49:11 +0100

    The Farm problem solved

---
+++ edabit-challenges/08-theFarmProblem.js
@@ -0,0 +1,8 @@
+function animals(chickens, cows, pigs) {
+  const chikenLegs = chickens * 2;
+  const cowsLegs = cows * 4;
+  const pigsLegs = pigs * 4;
+  const allLegs = chikenLegs + cowsLegs + pigsLegs
+  return allLegs
+}
+animals(2, 4, 4)
