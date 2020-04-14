125a463f3d6e764c164aa8a2ca839b92841d1a11
fouad97 <loukrichi21@gmail.com>
2020-04-14 23:12:41 +0100

    Average counter solved

---
+++ edabit-challenges/10-avergage.js
@@ -0,0 +1,20 @@
+// Write a function to find the average value in an array of numbers
+//avg([0,50]) //25
+//avg([75,76,80,95,100]) //85.2
+
+function avg(numbers) {
+	let sum = 0;
+	// numbers.forEach((num) => {
+	// 	sum += num;
+	// });
+	// for (let num = 0; num < numbers.length; num++) {
+	// 	sum += numbers[num];
+	// }
+	for (const num of numbers) {
+		sum += num;
+	}
+
+	return sum / numbers.length;
+}
+console.log(avg([0, 50]));
+console.log(avg([75, 76, 80, 95, 100]));
